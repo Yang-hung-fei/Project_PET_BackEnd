dff78e6f7b0dbbd3646e0f09bef7338ce5aa90b1
Yang-hung-fei <x52131221@gmail.com>
2023-10-09 23:48:06 +0800

    FanPay

--- src/main/java/project_pet_backEnd/productMall/order/dto/CallbackData.java
+++ src/main/java/project_pet_backEnd/productMall/order/dto/CallbackData.java
@@ -1,8 +1,10 @@
 package project_pet_backEnd.productMall.order.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CallbackData {
     private String action;
     private String paymentTransactionId;
